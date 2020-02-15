ca41986a8cc90c512786abce602de217f9c920f2
imaizumi <metallicaslayer2002@yahoo.co.jp>
2020-02-16 00:18:06 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'diaries/index'
-  get 'diaries/new'
-  get 'diaries/show'
-  get 'diaries/edit'
+resources :diaries
+root to: 'diaries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
