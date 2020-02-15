7cce1d9d08a343b9b24dbbbd08f66b9eab09846e
imaizumi <metallicaslayer2002@yahoo.co.jp>
2020-02-16 00:10:30 +0900

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'diaries/index'
+  get 'diaries/new'
+  get 'diaries/show'
+  get 'diaries/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
