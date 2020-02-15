15ff03df421c81cc576a90ea2ce5b02552bda535
imaizumi <metallicaslayer2002@yahoo.co.jp>
2020-02-16 01:29:03 +0900

    CRUD実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,13 +1,45 @@
 class DiariesController < ApplicationController
   def index
+    @diaries = Diary.all
   end
 
   def new
+    @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+
+    if @diary.save
+      redirect_to root_path, notice: "日記[#{@diary.title}]を登録しました"
+    else
+      render :new
+    end
   end
 
   def show
+    @diary = Diary.find(params[:id])
   end
 
   def edit
+    @diary = Diary.find(params[:id])
+  end
+
+  def update
+    diary = Diary.find(params[:id])
+    diary.update!(diary_params)
+    redirect_to root_path, notice: "日記[#{diary.title}]を編集しました。"
+  end
+
+  def destroy
+    diary = Diary.find(params[:id])
+    diary.destroy
+    redirect_to root_path, notice: "日記[#{diary.title}]をデストロイしました。"
+  end
+  
+  private
+
+  def diary_params
+    params.require(:diary).permit(:title, :description)
   end
 end
